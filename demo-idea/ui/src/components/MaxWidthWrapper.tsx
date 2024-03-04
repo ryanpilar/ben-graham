ac78d3f8f3c659b8d3ac3bfc4d486e9030c20321
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-04 15:30:48 -0700

    Spun up a nextjs starter and added a MaxWidth layout component

---
+++ demo-idea/ui/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+// Project Imports
+// 3rd Party Imports
+// Styles
+
+/** ================================|| Max Width Wrapper ||=================================== **/
+
+const MaxWidthWrapper = ({ className, children }: { className?: string, children: ReactNode }) => {
+    return (
+        <div className=''>
+        </div>
+    );
+};
+
+export default MaxWidthWrapper;
