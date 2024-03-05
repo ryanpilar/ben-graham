1b470266605da98cc048084c104376534acfb999
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-04 18:08:08 -0700

    Added lucide icons to project, and added a shadcn btn

--- demo-idea/ui/src/app/page.tsx
+++ demo-idea/ui/src/app/page.tsx
@@ -1,4 +1,13 @@
+import React from 'react'
+// Project Imports
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+// 3rd Party Imports
+import Link from "next/link";
+// Styles
+import {ArrowRight} from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button';
+
+/** ================================|| Home ||=================================== **/
 
 export default function Home() {
   return (
@@ -11,8 +20,34 @@ export default function Home() {
           hover:border-gray-300 hover:bg-white/50
           `
       }>
-        <p className='text-sm font-semibold text-gray-700'>Ben-g now public</p>
+
+        <p className='text-sm font-semibold text-gray-700'>Ben-G now public</p>
+
       </div>
+      <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+        Chat with your{' '}
+        <span className='text-blue-600'>documents</span>{' '}
+        in seconds.
+      </h1>
+
+      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+        Quill allows you to have conversations with any
+        PDF document. Simply upload your file and start
+        asking questions right away.
+      </p>
+
+      {/* /dashboard is a protected route, and will redirect to a login page if the user is not signed in */}
+      <Link
+        className={buttonVariants({
+          size: 'lg',
+          className: 'mt-5',
+        })}
+        href='/dashboard'
+        target='_blank'>
+        Get started{' '}
+        <ArrowRight className='h-5 w-5 ml-2' />
+      </Link>
+
     </MaxWidthWrapper>
   );
 }
