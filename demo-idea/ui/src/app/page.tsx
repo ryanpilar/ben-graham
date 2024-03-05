e6886c3be3c1c1739e0c934f2f1906e54afc75a0
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-04 17:34:56 -0700

    Updated tailwind config and global css with shadcn default styles

--- demo-idea/ui/src/app/page.tsx
+++ demo-idea/ui/src/app/page.tsx
@@ -3,7 +3,16 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 export default function Home() {
   return (
     <MaxWidthWrapper className='flex flex-col justify-center items-center mb-12 mt-28 sm:mt-40 text-center'>
-      Heloo
+      <div className={`
+          flex max-w-fit items-center justify-center 
+          space-x-2 mx-auto mb-4 px-7 py-2 overflow-hidden 
+          rounded-full border border-gray-200 bg-white 
+          shadow-md backdrop-blur transition-all 
+          hover:border-gray-300 hover:bg-white/50
+          `
+      }>
+        <p className='text-sm font-semibold text-gray-700'>Ben-g now public</p>
+      </div>
     </MaxWidthWrapper>
   );
 }
