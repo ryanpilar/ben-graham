50a0b269081a3f1dd6f516ddad18035103a60930
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-08 19:32:23 -0700

    Added toast from shadcn. Added upload thing hook snippes and started updating the UploadButton with the upload thing logic.

--- demo-idea/ui/src/app/api/uploadthing/core.ts
+++ demo-idea/ui/src/app/api/uploadthing/core.ts
@@ -1,34 +1,33 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
- 
+
 const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
- 
-// FileRouter for your app, can contain multiple FileRoutes
+
+/** ================================|| Core ||=================================== 
+    -   the middleware will run when someone has requested to upload a file from the client
+    -   onUploadComplete will run when the upload is successful 
+    -   only authenticated user can upload files
+**/
+
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
- 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
+
+    pdfUploader: f({ image: { maxFileSize: "4MB" } })
+        .middleware(async () => {
+            const { getUser } = getKindeServerSession()
+            const user = await getUser()
+          
+            if (!user || !user.id) throw new Error('Unauthorized')
+          
+            // const subscriptionPlan = await getUserSubscriptionPlan()
+          
+            // return { subscriptionPlan, userId: user.id }
+            return {kindeId: user.id} // whatever we return here, will end up below in metadata
+          })
+        .onUploadComplete(async ({ metadata, file }) => { }),
+
 } satisfies FileRouter;
- 
+
+// Uploadthing needs to know this type exported below. You can't just infer this type in the pkg.
+// therefore it expects on you to write this hook, but uploadthing supplies the snippet
 export type OurFileRouter = typeof ourFileRouter;
