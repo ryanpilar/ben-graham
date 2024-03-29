b446e73d75d31e0cd594c8adc731bffb127d827c
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-28 18:03:35 -0600

    PUT THEM BACK!!!

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -17,7 +17,9 @@ export const POST = async (req: NextRequest) => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
-  if (!user.id) return new Response('Unauthorized', { status: 401 })
+  // const { id: userId } = user
+  // if (!userId) return new Response('Unauthorized', { status: 401 })
+  if (!user?.id) return new Response('Unauthorized', { status: 401 })
 
   const { id: userId } = user
 
